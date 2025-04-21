bd18025bac613f32895b2de65651af25822057db
PunyawatCool <punyawat.mees@bumail.net>
2025-04-21 14:52:43 +0700

    เสร็จสมบูรณ์

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     
+    Vector2 moveInput; //for walk with addforce
+    
     //walk left-right
     private float move; //store Input from player
     [SerializeField] float speed; 
@@ -20,9 +22,13 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
-        //walk
+        //walk with addforce
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb2d.AddForce(moveInput * speed);
+        
+        /*//walk
         move = Input.GetAxis("Horizontal");
-        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocityY);
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocityY);*/
         
         //jump
         if (Input.GetButtonDown("Jump") && !isJumping)
