f670ed4a141c2442964f4d995276a553dead46ff
PunyawatCool <punyawat.mees@bumail.net>
2025-04-21 12:23:56 +0700

    ส่วนแรก

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
